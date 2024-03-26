4a07adef4d6e9a031d36824cc4c7a4c1334b9a5d
Nikhil Mankani <55614782+niksm7@users.noreply.github.com>
2024-03-26 14:04:14 +0530

    Create ImpalaToSQL.scala

---
+++ ImpalaToSQL.scala
@@ -0,0 +1,42 @@
+import org.apache.spark.sql.{SparkSession, DataFrame}
+
+object SparkToImpalaAndSQL {
+  def main(args: Array[String]): Unit = {
+    // Create a SparkSession
+    val spark = SparkSession.builder()
+      .appName("SparkToImpalaAndSQL")
+      .getOrCreate()
+
+    // Connect to Impala and fetch 10000 rows into DataFrame
+    val impalaDF: DataFrame = spark.read
+      .format("jdbc")
+      .option("url", "jdbc:impala://impala_host:21050/default")
+      .option("dbtable", "your_table")
+      .option("user", "username")
+      .option("password", "password")
+      .option("fetchsize", "10000") // Fetch 10000 rows at a time
+      .load()
+
+    // Show the schema of Impala DataFrame
+    impalaDF.printSchema()
+
+    // Connect to SQL server and save the DataFrame
+    val sqlUrl = "jdbc:mysql://sql_server_host:port/database"
+    val sqlTable = "sql_table"
+    val sqlUser = "username"
+    val sqlPassword = "password"
+
+    // Write Impala DataFrame to SQL
+    impalaDF.write
+      .format("jdbc")
+      .option("url", sqlUrl)
+      .option("dbtable", sqlTable)
+      .option("user", sqlUser)
+      .option("password", sqlPassword)
+      .mode("append") // Choose the appropriate mode
+      .save()
+
+    // Stop SparkSession
+    spark.stop()
+  }
+}
